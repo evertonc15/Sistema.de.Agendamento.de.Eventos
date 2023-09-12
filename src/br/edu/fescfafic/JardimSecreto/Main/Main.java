148e07418675f256ea3d33b144934028bf6c1d17
evertonc15 <everton.ci@outlook.com>
2023-09-12 08:29:20 -0300

    Classes criadas

--- src/br/edu/fescfafic/JardimSecreto/Main/Main.java
+++ src/br/edu/fescfafic/JardimSecreto/Main/Main.java
@@ -1,3 +1,5 @@
+package br.edu.fescfafic.JardimSecreto.Main;
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/br/edu/fescfafic/JardimSecreto/Model/Organizador.java
@@ -0,0 +1,4 @@
+package br.edu.fescfafic.JardimSecreto.Model;
+
+public class Organizador {
+}
