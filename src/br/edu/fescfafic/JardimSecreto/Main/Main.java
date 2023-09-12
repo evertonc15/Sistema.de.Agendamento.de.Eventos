0ca2d8874d5c81edfc25bb6f9c159c6d4ed474e8
Kaue Batista <kauecardozo3@gmail.com>
2023-09-12 20:27:39 -0300

    Atualização Organizador/Evento

--- src/br/edu/fescfafic/JardimSecreto/Main/Main.java
+++ src/br/edu/fescfafic/JardimSecreto/Main/Main.java
@@ -1,7 +1,22 @@
 package br.edu.fescfafic.JardimSecreto.Main;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import br.edu.fescfafic.JardimSecreto.Model.Evento;
+
 public class Main {
     public static void main(String[] args) {
+        Evento evento1 = new Evento(
+                10,
+                "Girls Night",
+                LocalDate.of(2003, 7, 7),
+                LocalTime.of(21, 10),
+                "Florianopolis",
+                10,
+                5
+        );
 
+        evento1.exibirInfo();
     }
 }
--- src/br/edu/fescfafic/JardimSecreto/Model/Organizador.java
+++ src/br/edu/fescfafic/JardimSecreto/Model/Organizador.java
@@ -1,4 +1,8 @@
 package br.edu.fescfafic.JardimSecreto.Model;
 
 public class Organizador {
+    public String nome;
+    public int id;
+    public String email;
+    public Organizador[] eventosOrganizados = new Organizador[3];
 }
