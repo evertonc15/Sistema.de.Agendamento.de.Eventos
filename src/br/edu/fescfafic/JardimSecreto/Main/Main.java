78ee00b771d862027972925379894f3500ab61c3
evertonc15 <everton.ci@outlook.com>
2023-09-18 17:48:29 -0300

    Por enquanto tudo funcionando de boa

--- src/br/edu/fescfafic/JardimSecreto/Main/Main.java
+++ src/br/edu/fescfafic/JardimSecreto/Main/Main.java
@@ -10,10 +10,17 @@ public class Main {
     public static void main(String[] args) {
         Evento evento = new Evento(
                 154626,
-                "Troca de cus",
+                "Cabare House",
                 LocalDate.of(2023,5,25),
                 LocalTime.of(17,20),
                 "Cajazeiras"
+        );
+        Evento evento1 = new Evento(
+                4564525,
+                "Drink Fest",
+                LocalDate.of(2023,5,28),
+                LocalTime.of(17,30),
+                "Cachoeira dos Indios"
         );
         //evento.exibirInfo();
         Organizador o1 = new Organizador(
@@ -23,11 +30,12 @@ public class Main {
         );
         o1.exibirInfo();
 
-        Calendario calendario = new Calendario(2);
-        calendario.inserirEvento(evento);
-        calendario.verificarDisponibilidade(evento);
-        System.out.println("+++++++++++++++++++++");
-        calendario.cancelarEvento(evento);
-        calendario.verificarDisponibilidade(evento);
+        Calendario calendario = new Calendario(5);
+        //calendario.inserirEvento(evento);
+        calendario.inserirEvento(evento1);
+        calendario.verificarDisponibilidade(evento1);
+//        System.out.println("+++++++++++++++++++++");
+//        calendario.cancelarEvento(evento);
+//        calendario.verificarDisponibilidade(evento);
     }
 }
--- src/br/edu/fescfafic/JardimSecreto/Model/Evento.java
+++ src/br/edu/fescfafic/JardimSecreto/Model/Evento.java
@@ -9,15 +9,15 @@ public class Evento {
     public LocalDate dataDoEvento;
     public LocalTime horaDoEvento;
     public String localDoEvento;
-    public int[] capacidadeDeParticipantes;
-    public int participantesRegistrados;
+    //public int[] capacidadeDeParticipantes;
+    //public int participantesRegistrados;
     public Evento(int id, String nomeDoEvento, LocalDate data, LocalTime hora, String local){
         this.idDoEvento = id;
         this.nomeDoEvento = nomeDoEvento;
         this.dataDoEvento = data;
         this.horaDoEvento = hora;
         this.localDoEvento = local;
-        this.capacidadeDeParticipantes = new int[participantesRegistrados];
+        //this.capacidadeDeParticipantes = new int[participantesRegistrados];
     }
     public void exibirInfo(){
         System.out.printf("ID: %s", this.idDoEvento);
@@ -25,6 +25,6 @@ public class Evento {
         System.out.printf("\nData: %s", this.dataDoEvento);
         System.out.printf("\nHora: %s", this.horaDoEvento);
         System.out.printf("\nLocal: %s", this.localDoEvento);
-        System.out.printf("\nTotal de partipantes: %s", this.participantesRegistrados);
+        //System.out.printf("\nTotal de partipantes: %s", this.participantesRegistrados);
     }
 }
